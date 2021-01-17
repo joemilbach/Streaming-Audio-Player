0da3c04742d980a91745e44f1e040b5f650637a0
Joe Milbach <joe.milbach@informaticsinc.com>
2021-01-17 15:36:09 -0600

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1><span>[</span><small>JM</small><span>]</span></h1>
+    </header>
+  );
+}
+
+export default Header;
