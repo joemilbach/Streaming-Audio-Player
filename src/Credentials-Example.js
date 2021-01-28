07f3b92f520ac035a2ff7c24764ee798de7ebd0a
Joe Milbach <joe.milbach@informaticsinc.com>
2021-01-27 20:50:35 -0600

    Clean up

---
+++ src/Credentials-Example.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+  return {
+    ClientId: "################################",
+    ClientSecret: "################################",
+    RedirectURI: "http://localhost:3000"
+  }
+}
+
+export { Credentials };
